e4ee06295ada7cb994cd97c271f8de36872423b3
Mohsen <as@ASs-iMac.local>
2024-05-18 18:40:37 +0530

    [REF] refactoring

---
+++ src/main/java/devpro/onlineshop/app/configs/JwtTokenUtils.java
@@ -0,0 +1,12 @@
+package devpro.onlineshop.app.configs;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class JwtTokenUtils implements Serializable {
+
+
+
+}
